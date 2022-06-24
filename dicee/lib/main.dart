47437f2f7111cc602d0a50641ed6996055190b63
vinaysantosh <vinaysantosh88@gmail.com>
2022-06-24 17:48:10 -0500

    initial commit

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,22 +16,52 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void setDiceNumber()
+  {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6)+1;
+      rightDiceNumber = Random().nextInt(6)+1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Image.asset(
-            'images/dice1.png',
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setDiceNumber();
+              },
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png',
+              ),
+            ),
           ),
-        ),
-        Expanded(
-          child: Image.asset(
-           'images/dice1.png'
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setDiceNumber();
+              },
+              child: Image.asset(
+                  'images/dice$rightDiceNumber.png'
+              ),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }

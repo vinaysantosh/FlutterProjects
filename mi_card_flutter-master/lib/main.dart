1d14cc386d5fa51eb5493ca3bbfbb9ce6e5333dd
vinaysantosh <vinaysantosh88@gmail.com>
2022-06-24 14:51:19 -0500

    nerw changes

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -10,9 +10,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: double.infinity,
+              width: 100.0,
+              color: Colors.red,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+            Container(
+              height: double.infinity,
+              width: 100.0,
+              color: Colors.blue,
+            )
+          ],
+        ),
       ),
+    ),
     );
   }
 }

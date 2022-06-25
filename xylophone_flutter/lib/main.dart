dd59b06221317557133a46d1f7fe0dc35e9a35ce
vinaysantosh <vinaysantosh88@gmail.com>
2022-06-24 21:03:23 -0500

    playing a single sound

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -1,14 +1,23 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  final player = AudioCache();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                player.play('note1.wav');
+              }, child: Text("Click Me!!"),
+            ),
+          ),
         ),
       ),
     );
